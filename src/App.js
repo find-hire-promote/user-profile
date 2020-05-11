b5a325fdd4d10ecec9a2be1e36ee59e1dffd13c6
Dan Woodson <woodson.dan@gmail.com>
2020-05-11 16:41:32 -0600

    Style tweaks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
-import "./styles.css";
-import Form from "./Form.js";
+import React from 'react'
+import './styles.css'
+import Form from './Form.js'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider
+} from '@material-ui/core/styles'
+import pink from '@material-ui/core/colors/pink'
+import green from '@material-ui/core/colors/green'
+
+const theme = createMuiTheme({
+  palette: {
+    primary: pink,
+    secondary: green
+  }
+})
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Your Profile</h1>
-      <Form />
-    </div>
-  );
+    <React.Fragment>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <h1>Your Profile</h1>
+          <Form />
+        </div>
+      </ThemeProvider>
+    </React.Fragment>
+  )
 }
